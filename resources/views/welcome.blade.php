2a7e80e0935d5ab122c448b5b168b5b342ce19be
sirpauley <sirpauley@gmail.com>
2018-06-15 12:22:22 +0200

    some authentication test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,21 +67,22 @@
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
+
+             @if (Auth::user()->username == 'admin') <!-- Test username = to admin for right to certain options -->
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
                     @endauth
                 </div>
+            @endif
+
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
                     Welcome
                     <br>
-                    {{ Auth::user()->email}}
+                    {{ Auth::user()->username}}
                 </div>
 
                 <div class="links">
